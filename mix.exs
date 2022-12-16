534473a59f3732945f0003ba037bb720b7652aab
Sergio Arbeo <serabe@gmail.com>
2022-12-16 01:19:34 +0100

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -26,6 +26,6 @@ defmodule AocHelpers.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
 end
